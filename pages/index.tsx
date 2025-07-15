f1ae46e10edb2661d30f37a3b94d32c45cd332e9
Breno Façanha <brenofacanha.ti@gmail.com>
2025-07-15 14:19:09 -0300

    excluindo exportacao inutilizada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import CardTecnology from "@/src/components/CardTecnology/CardTecnology";
 import Header from "@/src/components/Header/Header";
 import Sobre from "@/src/components/Sobre/Sobre";
 import Technology from "@/src/components/Technology/Technology";
-
-
 
 export default function Home() {
   return (
