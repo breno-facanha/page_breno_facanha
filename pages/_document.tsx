317c575b4f5751c8399217918f6fa9ecb9dd0b4a
Breno Façanha <brenofacanha.ti@gmail.com>
2025-08-05 16:20:33 -0300

    adicionando imagem e funcionalidade ao compartilhar link pelo whatsapp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Breno Façanha - Desenvolvedor Full Stack" />
+        <meta property="og:description" content="Desenvolvedor Full Stack especializado em criar soluções web modernas e eficientes. Transforme suas ideias em realidade com tecnologia de ponta." />
+        <meta property="og:image" content="/compatilhamento.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.png" /> 
       </Head>
